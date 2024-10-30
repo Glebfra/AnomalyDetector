627ca077d1cee8ceb3b83a69b03f8919505a31c0
glebfra <gleb.frantsuzov@yandex.ru>
2024-10-30 15:33:04 +0400

    updated variables in data generator

--- src/DataGenerator.py
+++ src/DataGenerator.py
@@ -7,9 +7,7 @@ class DataGenerator:
             points_number: int = 1000,
             trend_slope: float = 0.01,
             season_amplitude: float = 10,
-            season_period: float = 365,
-            periodic_amplitude: float = 5,
-            periodic_frequency: float = 1,
+            season_period: float = 60,
             random_noise_level: float = 1,
             random_seed: float = None
     ) -> None:
@@ -18,9 +16,6 @@ class DataGenerator:
 
         self.season_amplitude = season_amplitude
         self.season_period = season_period
-
-        self.periodic_amplitude = periodic_amplitude
-        self.periodic_frequency = periodic_frequency
 
         self.random_noise_level = random_noise_level
         if random_seed is not None:
@@ -39,13 +34,9 @@ class DataGenerator:
         return self.season_amplitude * np.sin(2 * np.pi * self.time / self.season_period)
 
     @property
-    def periodic_pattern(self) -> np.ndarray:
-        return self.periodic_amplitude * np.sin(2 * np.pi * self.periodic_frequency * self.time)
-
-    @property
     def random_noise(self) -> np.ndarray:
         return np.random.normal(0, self.random_noise_level, self.points_number)
 
     @property
     def data(self) -> np.ndarray:
-        return self.trend + self.seasonality + self.periodic_pattern + self.random_noise
+        return self.trend + self.seasonality + self.random_noise
