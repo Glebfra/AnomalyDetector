f260838e4678c518e6245e32269a545b2ae057e3
Glebfra <gleb.frantsuzov@yandex.ru>
2024-10-31 11:55:33 +0400

    added the documentation for methods

--- src/AnomalyDetector.py
+++ src/AnomalyDetector.py
@@ -6,12 +6,19 @@ from statsmodels.tsa.holtwinters import ExponentialSmoothing
 
 class AnomalyDetector:
     def __init__(self, window_size, seasonal_period=None, alpha=0.05) -> None:
+        """
+        This class used for anomaly detection
+        :param window_size: This param is used for moving average. This amount of first points will not be affected by the anomaly detection
+        :param seasonal_period: Leave None to calculate the seasonal period of the data, using Fast Fourier Transform
+        :param alpha: This param is used for calculating the lower and upper bounds of the anomaly detection
+        """
         self.history = []
         self.window_size = window_size
         self.seasonal_period = seasonal_period
         self.alpha = alpha
 
-    def update(self, new_point):
+    def update(self, new_point) -> bool:
+        """Updates the history of the anomaly detection and detects the anomaly of new_point"""
         self.history.append(new_point)
 
         if len(self.history) < self.window_size:
@@ -23,7 +30,7 @@ class AnomalyDetector:
         window_data = pd.Series(self.history[-self.window_size:])
         model = ExponentialSmoothing(
             window_data, trend='add', seasonal='add', seasonal_periods=self.seasonal_period
-            ).fit()
+        ).fit()
 
         forecast = model.forecast(steps=1).iloc[0]
 
@@ -37,7 +44,8 @@ class AnomalyDetector:
         is_anomaly = new_point < lower_bound or new_point > upper_bound
         return is_anomaly
 
-    def get_seasonal_period(self) -> float:
+    def get_seasonal_period(self) -> int:
+        """This method is used for calculating the seasonal period using Fast Fourier Transform"""
         history_fft = abs(fft(self.history) / len(self.history))
         frequency = fftfreq(history_fft.size)
         return int(1 / frequency[history_fft == max(history_fft)][0])
--- src/DataGenerator.py
+++ src/DataGenerator.py
@@ -1,5 +1,4 @@
 import random
-from time import sleep
 from typing import Generator
 
 import numpy as np
@@ -14,31 +13,38 @@ class DataGenerator:
             season_amplitude: float = 10,
             season_period: float = 60,
             random_noise_level: float = 1,
-            random_seed: float = None,
     ) -> None:
+        """
+        This class is used to generate data for anomaly detector based on trend slope, season and random points
+        :param trend_slope: The slope of the trend line
+        :param season_amplitude: The amplitude of season
+        :param season_period: The period of season
+        :param random_noise_level: The random noise level
+        """
         self.trend_slope = trend_slope
 
         self.season_amplitude = season_amplitude
         self.season_period = season_period
 
         self.random_noise_level = random_noise_level
-        if random_seed is not None:
-            random.seed(random_seed)
 
     def generate_data(self) -> Generator:
+        """Emulates the real time data"""
         while True:
             yield self.trend + self.seasonality + self.random_noise_point
             self.time += 1
 
     @property
     def trend(self) -> float:
+        """Creates the trend points based on the trend slope and time"""
         return self.trend_slope * self.time
 
     @property
     def seasonality(self) -> float:
+        """Creates the seasonality points between [-season_amplitude; season_amplitude]"""
         return self.season_amplitude * np.sin(2 * np.pi * self.time / self.season_period)
 
     @property
     def random_noise_point(self) -> float:
-        """Creates the random noise point between -noise_level and noise_level"""
+        """Creates the random noise point between [-noise_level; noise_level]"""
         return ((2 * random.random()) - 1) * self.random_noise_level
